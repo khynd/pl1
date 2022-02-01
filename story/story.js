4be65e977ea8036890ab4c8adb80af0d99740bbc
Kacy <91274036+khynd@users.noreply.github.com>
2022-02-01 15:40:22 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -2,7 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
+    // Made a mapID with google cloud platform-- remade Atlas style with lightened labels
+    mapId: "2d5f156f4e4ca58f",
     center: { lat: 45.055155, lng: -77.854767 },
     zoom: 8,
   });
