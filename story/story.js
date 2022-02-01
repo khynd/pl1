5a3f26872d42acb3451f4d3a1a972eb811a5d32f
Kacy <91274036+khynd@users.noreply.github.com>
2022-02-01 15:35:14 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -2,7 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "8e0a97af9386fef",
     center: { lat: 45.055155, lng: -77.854767 },
-    zoom: 6,
+    zoom: 8,
   });
 }
